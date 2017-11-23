a51583addcc5d8e31a0d6bfccfa9fcc8587fc485
Rubén Crespo-Cano <rcrespocano@gmail.com>
2017-11-23 09:36:38 +0100

    New python module: log module

---
+++ diversityrgc/log.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Helper utilities for logging."""
+
+import logging
+import os
+
+
+def create_logger(path, file_name='yarm.log'):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(name)s - %(message)s',
+                        handlers=[logging.FileHandler(path + file_name),
+                                  logging.StreamHandler()])
+
+
+def get_logger(name):
+    return logging.getLogger(name)
