b6f733cef50881ac669a97f68f9ef450496c4b3d
Rubén Crespo-Cano <rcrespocano@gmail.com>
2017-11-23 16:56:04 +0100

    Calculate Pearson correlation coefficient between each filter

--- diversityrgc/analyzer.py
+++ diversityrgc/analyzer.py
@@ -1,15 +1,60 @@
 # -*- coding: utf-8 -*-
 
+import os
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
 from . import ovndata
 from . import neuralnet
+from . import log
+
+
+# Logger
+logger = log.get_logger(__name__)
 
 
 def analyze(**kwargs):
+    out_folder = kwargs['output_folder']
+
     # Load data
     stim, sv = ovndata.load_data(**kwargs)
 
     # Convolutional neural network model for video classification trained on the Kinetics dataset
     net = neuralnet.NeuralNet(temporal_size=kwargs['temporal_size'],
                               image_size=kwargs['neural_net_image_size'],
-                              output_folder=kwargs['output_folder'])
-    net.run(stim, sv, nspikes=10)
+                              output_folder=out_folder)
+    net.run(stim, sv, nspikes=1000, save=True)
+
+    # Analyze data
+    files = [f for f in os.listdir(out_folder) if os.path.isfile(os.path.join(out_folder, f)) and f.endswith('.npy')]
+
+    # Accumulated Pearson correlation coefficient
+    logger.info('Calculate accumulated Pearson correlation coefficient')
+    pcc_size = np.load(out_folder + files[0]).shape[-1]
+    pcc = np.zeros(pcc_size)
+    counter = 0
+
+    for i in range(len(files)):
+        filters_x = np.load(out_folder + files[i])
+
+        for j in range(i+1, len(files)):
+            filters_y = np.load(out_folder + files[j])
+
+            for k in range(filters_x.shape[-1]):
+                x = filters_x[:, :, :, k].flatten()
+                y = filters_y[:, :, :, k].flatten()
+                pcc[k] += pearsoncc(x, y)
+
+            counter += 1
+
+    pcc /= counter
+
+    xaxis = np.arange(pcc.size)
+    plt.bar(xaxis, pcc)
+    plt.title('Pearson correlation coefficient')
+    plt.show()
+
+
+def pearsoncc(x, y):
+    cc = stats.pearsonr(x, y)[0]
+    return 0.0 if np.isnan(cc) else cc
