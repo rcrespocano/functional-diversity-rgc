f3015f7a22e65e0b9a95bb24f628a88a760136e7
Rubén Crespo-Cano <rcrespocano@gmail.com>
2017-11-23 14:38:21 +0100

    Use of argparse module to manage program arguments

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import time
+import argparse
 import datetime
 from diversityrgc import log, analyzer
 
@@ -14,11 +15,10 @@ if __name__ == '__main__':
     if sys.version_info[0] < 3:
         raise TypeError('This program must be executed with Python 3')
 
+    # Do not show TensorFlow warnings
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
     # Parameters
-    stim_dataset = '/home/rcc/Recordings/stim-pink-noise.mat'
-    bio_dataset = '/home/rcc/Recordings/20050825_serial_003.mat'
-    cell_type_index = 7
-    cell_number = 7
     trigger_frames = 100
     frame_size = 56
     temporal_size = 20
@@ -31,17 +31,39 @@ if __name__ == '__main__':
     # Info
     info = 'Functional caracterization of retinal ganglion cell diversity tool.'
 
+    parser = argparse.ArgumentParser(description=info)
+    parser.add_argument('--stim', metavar='stim', type=str, nargs='?', help='Input stimulus dataset')
+    parser.add_argument('--bio', metavar='bio', type=str, nargs='?', help='Input biological data')
+    parser.add_argument('--celltype', metavar='celltype', type=int, nargs='?', help='Cell type')
+    parser.add_argument('--cellnum', metavar='cellnum', type=int, nargs='?', help='Cell number')
+    args = parser.parse_args()
+
+    if args.stim is None:
+        raise TypeError('ERROR: the arg --stim is mandatory [Input stimulus dataset]')
+    if args.bio is None:
+        raise TypeError('ERROR: the arg --bio is mandatory [Input biological dat]')
+    if args.celltype is None:
+        raise TypeError('ERROR: the arg --celltype is mandatory [Cell type]')
+    if args.cellnum is None:
+        raise TypeError('ERROR: the arg --cellnum is mandatory [Cell number]')
+
     # Logger
     log.create_logger(output_folder)
     logger = log.get_logger(__name__)
     logger.info(info)
 
+    logger.info('Parameters:')
+    logger.info('> stim: %s', args.stim)
+    logger.info('> bio: %s', args.bio)
+    logger.info('> celltype: %s', args.celltype)
+    logger.info('> cellnum: %s', args.cellnum)
+
     # Parameters
     kwargs = dict()
-    kwargs['stim_dataset'] = stim_dataset
-    kwargs['bio_dataset'] = bio_dataset
-    kwargs['cell_type_index'] = cell_type_index
-    kwargs['cell_number'] = cell_number
+    kwargs['stim_dataset'] = args.stim
+    kwargs['bio_dataset'] = args.bio
+    kwargs['cell_type_index'] = args.celltype
+    kwargs['cell_number'] = args.cellnum
     kwargs['trigger_frames'] = trigger_frames
     kwargs['frame_size'] = frame_size
     kwargs['temporal_size'] = temporal_size
